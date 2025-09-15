6e8ca84c8c960bc09f9133e4d78978013b6d5a54
Arshia <arshiatn006@gmail.com>
2025-09-15 17:21:02 +0330

    updated and fixed admin.py in appointments

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,12 +1,18 @@
-from appointments.models import TimeSlot
 from django.contrib import admin
 from .models import Appointment, TimeSlot
+
 
 @admin.register(Appointment)
 class AppointmentAdmin(admin.ModelAdmin):
     list_display = ('id', 'patient', 'doctor', 'time_slot_info', 'status')
     list_filter = ('status', 'doctor', 'patient')
     search_fields = ('doctor__user__username', 'patient__username')
+
+    def time_slot_info(self, obj):
+        if obj.time_slot:
+            return f"{obj.time_slot.date} | {obj.time_slot.start_time} - {obj.time_slot.end_time}"
+        return "-"
+    time_slot_info.short_description = 'Time Slot'
 
 @admin.action(description="Mark selected slots as available")
 def make_available(modeladmin, request, queryset):
@@ -24,7 +30,3 @@ class TimeSlotAdmin(admin.ModelAdmin):
     list_filter = ("doctor", "date", "is_available")
     search_fields = ("doctor__user__first_name", "doctor__user__last_name", "doctor__license_number")
     actions = [make_available, make_unavailable]
-
-    def time_slot_info(self, obj):
-        return str(obj.time_slot)
-    time_slot_info.short_description = 'Time Slot'
