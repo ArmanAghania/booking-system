7d2679dbe0f69dab05427955af980359d024bd18
AmirhosseinMirjalili <amirmirjalili26@gmail.com>
2025-09-18 18:16:05 +0330

    Update models.py & create migrations files, admin.py

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from payments.models import Payment, WalletTransaction
 
-# Register your models here.
+@admin.register(WalletTransaction)
+class WalletTransactionAdmin(admin.ModelAdmin):
+    list_display = ['user_id_id','amount', 'transaction_type', 'balance_after']
+    list_filter = ['transaction_type']
+    search_fields = ['transaction_type']
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['appointment_id', 'amount', 'status']
+    search_fields = ['appointment_id']
